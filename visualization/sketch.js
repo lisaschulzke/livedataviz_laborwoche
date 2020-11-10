d577cc9ba25b51cd417c1c84127ada8382e98e01
Lisa Schulzke <56436771+lisaschulzke@users.noreply.github.com>
2020-11-10 13:16:43 +0100

    :100:  visualizing humidity as bubbles

--- visualization/sketch.js
+++ visualization/sketch.js
@@ -1,32 +1,74 @@
-let data = [];
+let data;
+let popy = [];
 
 function setup() {
     createCanvas(displayWidth, displayHeight);
     colorMode(HSB, 360, 100, 100);
 };
 
+function Bulle(x, y) {
+    this.x = x;
+    this.y = y;
+    this.diameter = 50;
+    this.speed = Math.random() + 1;
+    this.color = 'rgba(40,170,255,0.5)';
+    // this.colorRed = random(0, 255);
+    // this.colorGreen = random(0, 255);
+    // this.colorBlue = random(0, 255);
+    this.display = function () {
+        return ellipse(this.x, this.y, this.diameter, this.diameter)
+    }
+}
+
 function draw() {
     background(0);
-// stroke(30, 100, 100)
+    for (let i = 0; i < popy.length; i++) {
+        // fill(popy[i].colorRed, popy[i].colorGreen, popy[i].colorBlue);
+        fill(popy[i].color);
 
-// let xalt = 0;
-// let yalt = height;
+        popy[i].display();
 
-//     for (let i = 0; i < data.length; i++) {
-//         let d = data[i];
-//         let x = map(i, 1, 100, 0, width);
-//         let y = map(100, 0, 1000, height, 0);
 
-//         fill(30, 100, 100)
-//         circle(x, y, 5);
-//             stroke(30, 100, 100);
-//             line(xalt, yalt, x, y);
-
-//             xalt = x;
-//             yalt = y;
-//     }
-};
-
+        if (i % 2 == 0) {
+            popy[i].y += popy[i].speed;
+            popy[i].x += popy[i].speed;
+        } else {
+            popy[i].y += popy[i].speed;
+            popy[i].x -= popy[i].speed;
+        }
+        
+        if (i < 10){
+            popy[i].y += popy[i].speed;
+            popy[i].x += popy[i].speed;
+            popy[i].color = 'rgba(224, 176, 148, 1)';
+        } else if (10 >= i && i <= 20){
+            popy[i].y -= popy[i].speed;
+            popy[i].x -= popy[i].speed;
+            popy[i].color = 'rgba(71, 148, 230, 1)';
+        }else if (21 >= i && i <= 30){
+            popy[i].y += popy[i].speed;
+            popy[i].x += popy[i].speed;
+            popy[i].color = 'rgba(148, 121, 105, 1)';
+        }else if (31 >= i && i <= 40){
+            popy[i].y -= popy[i].speed;
+            popy[i].x += popy[i].speed;
+            popy[i].color = 'rgba(106, 138, 173, 1)';
+        }else if (41 >= i && i <= 50){
+            popy[i].y += popy[i].speed;
+            popy[i].x -= popy[i].speed;
+            popy[i].color = 'rgba(30, 62, 97, 1)';
+            console.log(i + " debug");
+        }else if (51 >= i && i <= 70){
+            popy[i].y += popy[i].speed;
+            popy[i].x -= popy[i].speed;
+            popy[i].color = 'rgba(137, 179, 224, 1)';
+        } else {
+            popy[i].y -= popy[i].speed;
+            popy[i].x += popy[i].speed;
+            popy[i].color = 'rgba(59, 77, 97, 1)';
+        }
+    }
+}
 
 let ws = new WebSocket("ws://localhost:1880/ws/receive")
 
@@ -35,10 +77,18 @@ ws.onopen = function (event) {
 };
 
 
-ws.onmessage = function(msg){
-    data.push(JSON.parse(msg.data));
-    if (data.length > 10) {
-        data.shift();
-    }
+ws.onmessage = function (msg) {
+    data = JSON.parse(msg.data);
+    popy = [];
+    // if (data.length > 10) {
+    //     data.shift();
+    // }
     console.log(data);
+    let humidityPopy = data.humidity;
+
+    for (let index = 0; index < humidityPopy; index++) {
+        popy.push(new Bulle(Math.random() * 800 + index, Math.random() * 800 + index));
+    };
+    draw();
+    // let humidityPopy = msg.humidity;
 };
