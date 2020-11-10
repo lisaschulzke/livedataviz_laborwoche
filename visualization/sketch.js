73b360573bdae41116977db778a9dfd93313fb57
Lisa Schulzke <56436771+lisaschulzke@users.noreply.github.com>
2020-11-10 10:42:23 +0100

    :sparkles: adding p5 elements to code

--- visualization/sketch.js
+++ visualization/sketch.js
@@ -1,3 +1,33 @@
+let data = [];
+
+function setup() {
+    createCanvas(displayWidth, displayHeight);
+    colorMode(HSB, 360, 100, 100);
+};
+
+function draw() {
+    background(0);
+// stroke(30, 100, 100)
+
+// let xalt = 0;
+// let yalt = height;
+
+//     for (let i = 0; i < data.length; i++) {
+//         let d = data[i];
+//         let x = map(i, 1, 100, 0, width);
+//         let y = map(100, 0, 1000, height, 0);
+
+//         fill(30, 100, 100)
+//         circle(x, y, 5);
+//             stroke(30, 100, 100);
+//             line(xalt, yalt, x, y);
+
+//             xalt = x;
+//             yalt = y;
+//     }
+};
+
+
 let ws = new WebSocket("ws://localhost:1880/ws/receive")
 
 ws.onopen = function (event) {
@@ -6,5 +36,9 @@ ws.onopen = function (event) {
 
 
 ws.onmessage = function(msg){
-    console.log(msg);
+    data.push(JSON.parse(msg.data));
+    if (data.length > 10) {
+        data.shift();
+    }
+    console.log(data);
 };
